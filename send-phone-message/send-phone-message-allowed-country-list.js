b7cbb9050c925ae9e22e87aad9ee48c8f7611ff5
Vikas Kannurpatti Jayaram <vikas.ramasethu@gmail.com>
2023-12-20 13:12:43 +1100

    checked allowed country to send sms for passwordless and MFA (#8)

---
+++ send-phone-message/send-phone-message-allowed-country-list.js
@@ -0,0 +1,83 @@
+/**
+ * Send Phone Message MFA with Twilio
+ *
+ * Author: Vikas Jayaram <vikas@okta.com>
+ * Date: 2023-11-24
+ * License: MIT (https://github.com/auth0/actions-galleryh/blob/main/LICENSE)
+ *
+ * @param {Event} event - Details about the user and the context in which they are logging in.
+ * @param {SendPhoneMessageAPI} api - Methods and utilities to help change the behavior of sending a phone message.
+*/
+const PhoneNumber = require('awesome-phonenumber');
+exports.onExecuteSendPhoneMessage = async (event, api) => {
+    const accountSid = event.secrets.TWILIO_ACCOUNT_SID;
+    const authToken = event.secrets.TWILIO_AUTH_TOKEN;
+    const fromPhoneNumber = event.secrets.TWILIO_PHONE_NUMBER;
+
+    const twilioClient = require("twilio")(accountSid, authToken);
+    const { action, text, recipient, message_type } = event.message_options;
+    const ALLOWED_COUNTRY_CODES = ["AU"];
+   var pn = new PhoneNumber(recipient); 
+    var mfaUser = {
+        action: action,
+        message_type: message_type,
+        country_code: pn.getCountryCode(),
+        country: pn.getRegionCode(),
+        phone_number: pn.getNumber('significant'),
+        e164_phone_number: pn.getNumber(),
+        auth0_user_id: event.user.user_id,
+        email: event.user.email
+    };
+
+    // send mfaUser to splunk / sumo for logging.
+    
+    async function isEnrollmentAllowed(countryCode) {
+        return ALLOWED_COUNTRY_CODES.includes(countryCode);
+    }
+
+    async function sendSMSToGateway(body, from, to) {
+        return new Promise((resolve, reject) => {
+            twilioClient.messages.create({
+                body: body,
+                from: from,
+                to: to,
+            }).then(message => {
+                resolve(message);
+            }).catch(error => {
+                reject(error);
+            });
+        })
+    }
+
+    async function enrollment(countryCode, body, from, to){
+        try {
+            var isAllowed = await isEnrollmentAllowed(countryCode);
+            if (isAllowed) {
+                var messageResponse = await sendSMSToGateway(body, from, to);
+            } else {
+                var errMsg = `Someone tried to enroll with phone number ${recipient}  from country ${countryCode}`;
+                throw new Error(errMsg);
+            }
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
+
+    async function send2FA (countryCode, body, from, to) {
+        try {
+            var messageResponse = await sendSMSToGateway(body, from, to);
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
+    switch (action) {
+        case 'enrollment':
+            await enrollment(pn.getRegionCode(), text, fromPhoneNumber, recipient);
+            break;
+        case 'second-factor-authentication':
+            await send2FA(pn.getRegionCode(), text, fromPhoneNumber, recipient);
+            break;
+        default:
+            break;
+    }
+};
